7a6f3aa25c6b5e51b5c726aaefab381467ab51c6
Alexandre Lairan <lairana@free.fr>
2017-12-27 18:15:58 +0100

    Add contracts

---
+++ app/concepts/classroom/contract/create.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Classroom::Contract
+  class Create < Reform::Form
+  end
+end
---
+++ app/concepts/classroom/contract/update.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Classroom::Contract
+  class Update < Reform::Form
+  end
+end
